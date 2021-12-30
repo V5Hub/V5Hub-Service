9325207b3b4999bbedae8090d2d10817bd64c98b
WarmCongee <1979557180@qq.com>
2021-12-31 01:10:01 +0800

    code: add entity class of database

---
+++ src/main/java/com/example/volunteerservice/mapper/ActivityMapper.java
@@ -0,0 +1,10 @@
+package com.example.volunteerservice.mapper;
+
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.example.volunteerservice.module.Activity;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface ActivityMapper extends BaseMapper<Activity> {
+}
