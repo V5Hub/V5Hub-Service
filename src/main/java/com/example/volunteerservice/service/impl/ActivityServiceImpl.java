462094dddb3a47c1aa8986e3e496c437359fe69d
WarmCongee <1979557180@qq.com>
2022-01-01 18:16:51 +0800

    add service class

---
+++ src/main/java/com/example/volunteerservice/service/impl/ActivityServiceImpl.java
@@ -0,0 +1,10 @@
+package com.example.volunteerservice.service.impl;
+
+import com.example.volunteerservice.service.ActivityService;
+
+/**
+ * @author WarmCongee
+ * @date 2022/1/1 18:14
+ */
+public class ActivityServiceImpl implements ActivityService {
+}
