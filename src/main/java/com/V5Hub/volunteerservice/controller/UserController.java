ad9f23262724be47d7ba4ee8af14f5eabbcd9244
KirCute <951206789@qq.com>
2022-01-11 00:08:59 +0800

    Interface of getting user information

--- src/main/java/com/V5Hub/volunteerservice/controller/UserController.java
+++ src/main/java/com/V5Hub/volunteerservice/controller/UserController.java
@@ -1,6 +1,10 @@
 package com.V5Hub.volunteerservice.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import com.V5Hub.volunteerservice.model.User;
+import com.V5Hub.volunteerservice.service.UserService;
+import com.V5Hub.volunteerservice.util.response.Result;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * 普通用户Controller
@@ -9,4 +13,23 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 public class UserController {
+    private final UserService userService;
+
+    @Autowired
+    public UserController(UserService userService){
+        this.userService = userService;
+    }
+
+    @RequestMapping(value = "/wx/user", method = RequestMethod.GET)
+    @ResponseBody
+    public Result info(@RequestParam(value = "openid", defaultValue = "") String openid) {
+        User user = userService.selectById(openid);
+        if (user == null) {
+            return new Result(null, 404, "Invalid openid.");
+        }
+        if (user.isDeleted()) {
+            return new Result(null, 403, "Requested user has been deleted.");
+        }
+        return new Result(user, 200, "Success.");
+    }
 }
