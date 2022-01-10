8ad3b5c68bf328e52754310e85eeb67a2480339d
KirCute <951206789@qq.com>
2022-01-11 00:25:55 +0800

    Interface of user information edit

--- src/main/java/com/V5Hub/volunteerservice/controller/UserController.java
+++ src/main/java/com/V5Hub/volunteerservice/controller/UserController.java
@@ -22,14 +22,44 @@ public class UserController {
 
     @RequestMapping(value = "/wx/user", method = RequestMethod.GET)
     @ResponseBody
-    public Result info(@RequestParam(value = "openid", defaultValue = "") String openid) {
+    public Result getInfo(@RequestParam(value = "openid", defaultValue = "") String openid) {
         User user = userService.selectById(openid);
         if (user == null) {
             return new Result(null, 404, "Invalid openid.");
         }
         if (user.isDeleted()) {
-            return new Result(null, 403, "Requested user has been deleted.");
+            return new Result(null, 410, "Requested user has been deleted.");
         }
+        user.setPassword("");
+        user.setPicture(null);
+        user.setRegisters(null);
         return new Result(user, 200, "Success.");
+    }
+
+    @RequestMapping(value = "/user", method = RequestMethod.POST)
+    @ResponseBody
+    public Result setInfo(@RequestParam(value = "openid", defaultValue = "") String openid,
+                          @RequestParam(value = "name", defaultValue = "") String name,
+                          @RequestParam(value = "studentId", defaultValue = "") String studentId,
+                          @RequestParam(value = "email", defaultValue = "") String email,
+                          @RequestParam(value = "phoneNumber", defaultValue = "") String phoneNumber,
+                          @RequestParam(value = "college", defaultValue = "") String college) {
+        User user = userService.selectById(openid);
+        if (user == null) {
+            return new Result(null, 404, "Invalid openid.");
+        }
+        if (user.isDeleted()) {
+            return new Result(null, 410, "Requested user has been deleted.");
+        }
+        user.setName(name);
+        user.setStudentId(studentId);
+        user.setEmail(email);
+        user.setPhoneNumber(phoneNumber);
+        user.setCollege(college);
+        if (userService.update(user) == 0) {
+            return new Result(null, 500, "Update failed.");
+        } else {
+            return new Result(null, 200, "Success.");
+        }
     }
 }
