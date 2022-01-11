1e9d7659a6513568b887e9471bf1a46b1b0557a5
KirCute <951206789@qq.com>
2022-01-11 22:01:32 +0800

    Interface of recent activities

--- src/main/java/com/V5Hub/volunteerservice/controller/ActivityController.java
+++ src/main/java/com/V5Hub/volunteerservice/controller/ActivityController.java
@@ -1,10 +1,12 @@
 package com.V5Hub.volunteerservice.controller;
 
+import com.V5Hub.volunteerservice.model.Activity;
 import com.V5Hub.volunteerservice.service.ActivityService;
+import com.V5Hub.volunteerservice.util.response.Result;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.*;
 
 /**
  * 活动Controller
@@ -18,5 +20,26 @@ public class ActivityController {
     @Autowired
     public ActivityController(ActivityService activityService) {
         this.activityService = activityService;
+    }
+
+    @RequestMapping(value = "/activities/recent", method = RequestMethod.GET)
+    @ResponseBody
+    public Result recent(@RequestParam(value = "openid", defaultValue = "") String openid) {
+        Calendar date = new GregorianCalendar();
+        date.setTime(new Date());
+        date.add(Calendar.DATE, -6);
+        ArrayList<HashMap<String, Object>> data = new ArrayList<>();
+        for (int i = -6; i <= 21; i++) {
+            HashMap<String, Object> item = new HashMap<>();
+            item.put("date", String.format("%d-%d-%d",
+                    date.get(Calendar.YEAR), date.get(Calendar.MONTH) + 1, date.get(Calendar.DAY_OF_MONTH)));
+            Date pre = date.getTime();
+            date.add(Calendar.DATE, 1);
+            List<Activity> activities = activityService.selectByDate(pre, date.getTime());
+            // TODO: 根据用户对tag的订阅情况对activity.tags做进一步处理
+            item.put("activities", activities);
+            data.add(item);
+        }
+        return new Result(data, 200, "Success.");
     }
 }
